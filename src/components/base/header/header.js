2627e8c5437e45c1045af8b4860be51d1b11e90f
Vanushka <30829997+Vanushka@users.noreply.github.com>
2019-08-28 20:29:52 +0300

    edit mobile menu

--- src/components/base/header/header.js
+++ src/components/base/header/header.js
@@ -12,6 +12,30 @@ import logoStandart from './img/svg/logo.svg';
 import logoWhite from './img/svg/logoWhite.svg';
 
 class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})
+  }
+
+  // This can be used to close the menu, e.g. when a user clicks a menu item
+  closeMenu () {
+    this.setState({menuOpen: false})
+  }
+
+  // This can be used to toggle the menu, e.g. when using a custom icon
+  // Tip: You probably want to hide either/both default icons if using a custom icon
+  // See https://github.com/negomi/react-burger-menu#custom-icons
+  toggleMenu () {
+    this.setState(state => ({menuOpen: !state.menuOpen}))
+  }
   render () {
     let logo = location.pathname === "/what_are_we_doing" ? logoWhite : (location.pathname === "/contacts" ? logoWhite : logoStandart);
     function changeColor() {
@@ -39,26 +63,35 @@ class Header extends React.Component {
     return (
       <header>
         <div className="burger">
-          <Menu>
+          <Menu isOpen={this.state.menuOpen}
+            onStateChange={(state) => this.handleStateChange(state)}
+          >
             <Link
+              onClick={() => this.closeMenu()}
+              spy={true}
               activeClass="active"
               smooth={true}
               duration= {1000}
               to='we_made'>Порфтолио
             </Link>
             <Link
+              onClick={() => this.closeMenu()}
+              spy={true}
               activeClass="active"
               smooth={true}
               duration= {1000}
               to='what_are_we_doing'>Что мы умеем
             </Link>
             <Link
+              onClick={() => this.closeMenu()}
+              spy={true}
               activeClass="active"
               smooth={true}
               duration= {1000}
               to='who_do_we_do'>Клиенты
             </Link>
             <Link
+              onClick={() => this.closeMenu()}
               spy={true}
               activeClass="active"
               smooth={true}
